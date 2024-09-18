12c8fd6adc7e4c2736260b3b45ed30ad180167e8
Erfan Karimi <erfan.karimi@snapp.cab>
2024-09-18 22:01:30 +0330

    feat: The project initialized.

---
+++ src/test/java/com/karimi/erfan/to_do_list_api/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karimi.erfan.to_do_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
